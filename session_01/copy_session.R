9dad4dd000c8c759a1af6ff85fcf61fc2ea4e86f
Danny Smith <danny@gorcha.org>
2022-11-03 12:29:40 +1100

    Add qmd source for slides

--- session_01/copy_session.R
+++
@@ -1,7 +0,0 @@
-
-copy_session <- function(session) {
-  file.copy(paste0("session_", session), "../r_shiny_intro_data/", overwrite = TRUE, recursive = TRUE, copy.date = TRUE)
-  file.copy(list.files(pattern = paste0("^", session, "-.*html")), "../r_shiny_intro_data/slides/", overwrite = TRUE, recursive = TRUE, copy.date = TRUE)
-}
-
-copy_session("01")
